160f9730dfdb4e9f0a92a42256a52e956ee37c24
Skirmant <Skirmant@gmx.com>
2015-01-07 14:02:59 +0200

    change dust limit

--- core/src/main/java/com/google/bitcoin/core/CoinDefinition.java
+++ core/src/main/java/com/google/bitcoin/core/CoinDefinition.java
@@ -120,7 +120,7 @@ public class CoinDefinition {
     //public static final String MAX_MONEY_STRING = "200000000";     //main.h:  MAX_MONEY
 
     public static final BigInteger DEFAULT_MIN_TX_FEE = BigInteger.valueOf(1000);   // MIN_TX_FEE
-    public static final BigInteger DUST_LIMIT = BigInteger.valueOf(1000000); //main.h CTransaction::GetMinFee        0.01 coins
+    public static final BigInteger DUST_LIMIT = BigInteger.valueOf(100); //main.h CTransaction::GetMinFee        0.01 coins
 
     public static final int PROTOCOL_VERSION = 70001;          //version.h PROTOCOL_VERSION
     public static final int MIN_PROTOCOL_VERSION = 60002;        //version.h MIN_PROTO_VERSION - eliminate 60001 which are on the wrong fork
